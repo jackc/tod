beb9355de986462017df58f195123e5275c99d28
Tom Close <tom.close@cantab.net>
2014-02-27 14:50:32 +0000

    Make time of day compatible with MongoDB
    This should probably actually be in a rails-tod gem (cf. rails-money).
    As it is, in order to test I would have to have a dummy rails app in the,
    library, which doesn't seem right ...

--- lib/tod.rb
+++ lib/tod.rb
@@ -3,6 +3,11 @@ require 'tod/date'
 require 'tod/time'
 require 'tod/shift'
 require 'tod/conversions'
+require 'tod/mongoization'
 
 include Tod
 include Tod::Conversions
+class TimeOfDay
+  include(Tod::Mongoization)
+  extend(Tod::Mongoization::ClassMethods)
+end
