bbd67725be7897f0f77a23f136da9e2891b440e7
Jack Christensen <jack@jackchristensen.com>
2015-05-19 18:56:49 -0500

    Improve test to ensure records are successfully created

--- test/tod/time_of_day_serializable_attribute_test.rb
+++ test/tod/time_of_day_serializable_attribute_test.rb
@@ -8,23 +8,26 @@ end
 describe "TimeOfDay with ActiveRecord Serializable Attribute" do
   describe ".dump" do
     it "sets time of day" do
-      Order.create(time: Tod::TimeOfDay.new(9, 30))
+      Order.create!(time: Tod::TimeOfDay.new(9, 30))
     end
 
     it "sets nil as value" do
-      Order.create(time: nil)
+      Order.create!(time: nil)
     end
   end
 
   describe ".load" do
     it "loads set time" do
       time_of_day = Tod::TimeOfDay.new(9, 30)
-      order = Order.create(time: time_of_day)
+      order = Order.create!(time: time_of_day)
+      binding.pry
+      order.reload
       assert_equal order.time, time_of_day
     end
 
     it "returns nil if time is not set" do
-      order = Order.create(time: nil)
+      order = Order.create!(time: nil)
+      order.reload
       assert_equal order.time, nil
     end
   end
