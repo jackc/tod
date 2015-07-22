b14e8c0380921ccdb322a510c6d5a45d6d0add3b
Paul Tyng <paul@paultyng.net>
2015-07-22 10:31:29 -0400

    Add shift slide method

--- lib/tod/shift.rb
+++ lib/tod/shift.rb
@@ -62,5 +62,10 @@ module Tod
     def exclude_end?
       @exclude_end
     end
+    
+    # Move start and end by a number of seconds and return new shift.
+    def slide(seconds)
+      self.class.new(beginning + seconds, ending + seconds, exclude_end?)
+    end
   end
 end
