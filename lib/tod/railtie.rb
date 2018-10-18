d83593c2d2d57c7e5b1e3cf997eedc202005c7b2
Steve Hoeksema <steve@kotiri.com>
2018-10-19 11:33:31 +1300

    Depend on ActiveModel instead of ActiveRecord
    So that Tod will continue to work in Rails apps that don't have ActiveRecord enabled

--- lib/tod/railtie.rb
+++ lib/tod/railtie.rb
@@ -1,9 +1,10 @@
+require "active_model/type"
 require "tod/time_of_day_type"
 
 module Tod
   class Railtie < Rails::Railtie
-    initializer "tod.register_active_record_type" do
-      ActiveRecord::Type.register(:time_only, Tod::TimeOfDayType)
+    initializer "tod.register_active_model_type" do
+      ActiveModel::Type.register(:time_only, Tod::TimeOfDayType)
     end
   end
 end
