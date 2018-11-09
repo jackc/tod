ac1dc9469a70a46717d1d5499b71af4663544c44
Jack Christensen <jack@jackchristensen.com>
2018-11-08 19:56:45 -0600

    Only register ActiveModel type on Rails 5

--- lib/tod/railtie.rb
+++ lib/tod/railtie.rb
@@ -4,7 +4,11 @@ require "tod/time_of_day_type"
 module Tod
   class Railtie < Rails::Railtie
     initializer "tod.register_active_model_type" do
-      ActiveModel::Type.register(:time_only, Tod::TimeOfDayType)
+      if Rails.version[0] > "4"
+        ActiveModel::Type.register(:time_only, Tod::TimeOfDayType)
+      else
+        ActiveRecord::Type.register(:time_only, Tod::TimeOfDayType)
+      end
     end
   end
 end
