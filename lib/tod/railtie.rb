f7bbbf26415cbd501eb9eb499df80002ba182a5f
Damir Zekić <damirz@gmail.com>
2021-03-03 18:58:15 +0100

    Register type to both ActiveModel and ActiveRecord
    Attributes API was introduced in Rails 5, so
    `ActiveRecord::Type.register(:time_only, Tod::TimeOfDayType)` is only
    supported since Rails 5—it's not a fallback for older versions of Rails.
    
    In Rails 6.1 we ran into an issue where a type registered in
    ActiveModel's registry was not available in ActiveRecord models. This
    might be a change in Rails or load order quirk. In any case code
    examples in the documentation suggest that a type should be registered
    in both registries.

--- lib/tod/railtie.rb
+++ lib/tod/railtie.rb
@@ -8,7 +8,6 @@ module Tod
     initializer "tod.register_active_model_type" do
       if Rails.version[0] > "4"
         ActiveModel::Type.register(:time_only, Tod::TimeOfDayType)
-      else
         ActiveRecord::Type.register(:time_only, Tod::TimeOfDayType)
       end
     end
